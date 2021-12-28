9f41660bf85008d33c31a1b701e7cb39e476b248
Seohyun Lee <studioseorii@gmail.com>
2021-12-29 01:01:28 +0900

    docs: add README, favicon

---
+++ README.md
@@ -0,0 +1,13 @@
+## HANA
+
+<img src="res/logo.png" width="200" alt="logo">
+
+HANA is a PS Judgement Server that runs on Docker.
+
+## Installation
+
+TBA
+
+## Usage
+
+You have to use both websocket and http request to use HANA.
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from "express"
 import expressWs from "express-ws"
 import {v4 as uuid} from "uuid"
-import {sendMessage, init} from "./socket";
+import {init} from "./socket";
 import {requestJudge} from "./judge";
 import {Problem} from "./types/problem";
+import favicon from 'serve-favicon'
 
 
 const app = expressWs(express()).app;
@@ -20,5 +21,7 @@ app.get('/judge', (req, res) => {
 app.get('/', function (req, res) {
     res.send('HANA v1.0');
 });
+
+app.use(favicon(__dirname + '/../res/logo.ico'));
 
 app.listen(80);
