3cee25a8aab9312f2ca34459412295cccf6d4c05
Seohyun Lee <studioseorii@gmail.com>
2021-12-29 15:28:42 +0900

    fix: add source type

--- src/types/request.ts
+++ src/types/request.ts
@@ -7,3 +7,28 @@ export interface WebSocketRequest {
     type: WebSocketRequestType
     data?: any
 }
+
+export const enum JudgeSourceType {
+    TEXT = 'TEXT',
+    C = 'C',
+    CPP = 'CPP',
+    PYTHON3 = 'PYTHON3',
+    PYPY = 'PYPY',
+    JAVA = 'JAVA',
+    JAVASCRIPT = 'JAVASCRIPT',
+}
+
+export interface SourceFile {
+    name: string
+    ext: JudgeSourceType
+    source: string
+}
+
+export interface JudgeRequest {
+    language: JudgeSourceType
+    source: string
+    input: string[]
+    output: string[]
+    timeLimit: number
+    memoryLimit: number
+}
