6ade9531ecc3a56236dc10b0e64d42315a579d73
Seohyun Lee <studioseorii@gmail.com>
2021-12-29 11:19:12 +0900

    feat: keep message on sent fail

--- src/socket.ts
+++ src/socket.ts
@@ -5,6 +5,7 @@ import {WebSocketRequest, WebSocketRequestType} from "./types/request";
 import {getJudgeInfo} from "./judge";
 
 let wsObject = null as WebSocket | null
+let messageList = [] as string[]
 
 export function sendMessage(type: WebSocketResponseType, data?: any) {
     if (wsObject) {
@@ -17,18 +18,29 @@ export function sendMessage(type: WebSocketResponseType, data?: any) {
 }
 
 export function sendError(reason: string) {
+    const message = JSON.stringify({
+        success: false,
+        error: reason
+    } as WebSocketResponse)
     if (wsObject) {
-        wsObject.send(JSON.stringify({
-            success: false,
-            error: reason
-        } as WebSocketResponse));
-    }
+        wsObject.send(message, () => {
+            if (wsObject) {
+                wsObject.close();
+                wsObject = null;
+            }
+            messageList.push(message)
+        });
+    } else messageList.push(message)
 }
 
 export function init(app: expressWs.Application) {
     app.ws('/', (ws, req) => {
         if (wsObject) wsObject.close()
         wsObject = ws
+        for (const message of messageList) {
+            wsObject.send(message)
+        }
+        messageList = []
         sendMessage(WebSocketResponseType.JUDGE_STATUS, getJudgeInfo())
         ws.on('message', function (msg) {
             try {
