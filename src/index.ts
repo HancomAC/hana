961d11c820dddda8495d8dec20b6a9d4598ff27a
Seohyun Lee <studioseorii@gmail.com>
2022-01-07 00:15:25 +0900

    feat: make main process to not die

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-import './app'
+import * as path from 'path'
+import { execSync } from 'child_process'
+
+const rootPath = path.join(__dirname, '..')
+const appPath = path.join(__dirname, 'app.js')
+const appCommand = `node --es-module-specifier-resolution=node ${appPath}`
+
+while (true) {
+    try {
+        execSync(appCommand, { cwd: rootPath, stdio: 'inherit' })
+    } catch (e) {}
+}
